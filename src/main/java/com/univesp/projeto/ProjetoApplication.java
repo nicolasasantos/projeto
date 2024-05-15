6a583284785b7c15c214919bbe8ce91ba4220b79
MatheusMagalhaesz <matheusop806@gmail.com>
2024-05-15 16:40:51 -0300

    Added a blank space in function println to commit and push

--- src/main/java/com/univesp/projeto/ProjetoApplication.java
+++ src/main/java/com/univesp/projeto/ProjetoApplication.java
@@ -9,7 +9,7 @@ public class ProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Hello World!   ");
 	}
 
 
